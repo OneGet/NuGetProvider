0e4111ae238c3f01dd43956d7e01a67f7258153c
Quoc Truong <quoct@microsoft.com>
2015-10-06 12:21:25 -0700

    Add AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Microsoft.PackageManagement.NuGetProvider")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NuGet Provider for OneGet")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyProduct("Microsoft.PackageManagement.NuGetProvider")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
