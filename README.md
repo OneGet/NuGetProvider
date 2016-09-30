590d286729f6f208c4b72b03c6828a0ec41062bf
Jianyun Tao <jianyunt@microsoft.com>
2016-09-29 18:37:59 -0700

    Added smoke tests

---
+++ README.md
@@ -0,0 +1,19 @@
+NuGet provider
+==============
+
+NuGetProvider is a OneGet (PackageManagement) provider.
+It primarily supports finding and installing packages from [NuGet.org][nugetorg].
+It is also used by [PowerShellGet][psget] for finding and installing PowerShell modules from [PowerShellGallery][psgallery].
+
+It supports FullCLR and CoreCLR, meaning works for Windows, Linux, and OSX as part of [PowerShellCore][pscore].
+
+As a git submodule, it is compiled and tested with [OneGet][oneget] together.
+
+For more information, see [OneGet wiki][wiki].
+
+[nugetorg]: https://www.nuget.org
+[psgallery]: https://www.PowerShellGallery.com
+[psget]: https://github.com/PowerShell/PowerShellget
+[oneget]: https://www.oneget.org
+[pscore]: https://github.com/PowerShell/PowerShell
+[wiki]: https://github.com/oneget/oneget/wiki
---
+++ smoketest.tests.ps1
@@ -0,0 +1,35 @@
+﻿
+Describe "Smoke testing" -Tags "Feature" {
+ 
+ BeforeAll{
+    Register-PackageSource -Name Nugettest -provider NuGet -Location https://www.nuget.org/api/v2 -force
+
+ }
+
+    it "EXPECTED: Find a package"  {
+        $a = Find-Package -ProviderName NuGet -Name jquery -source Nugettest
+        $a.Name -contains "jquery" | should be $true
+	}
+
+    it "EXPECTED: Install a package"  {
+        $a = install-Package -ProviderName NuGet -Name jquery -force -source Nugettest
+        $a.Name -contains "jquery" | should be $true
+	}
+
+
+    it "EXPECTED: Get a package"  {
+        $a = Get-Package -ProviderName NuGet -Name jquery
+        $a.Name -contains "jquery" | should be $true
+	}
+
+    it "EXPECTED: save a package"  {
+        $a = save-Package -ProviderName NuGet -Name jquery -path $TestDrive -force -source Nugettest
+        $a.Name -contains "jquery" | should be $true
+	}
+
+    it "EXPECTED: uninstall a package"  {
+        $a = uninstall-Package -ProviderName NuGet -Name jquery
+        $a.Name -contains "jquery" | should be $true
+	}
+
+}
