5bf937d437c5a648d516134af75b7dc7e49fe8e4
Jianyun Tao <jianyunt@microsoft.com>
2016-11-08 09:39:05 -0800

    Bump up the Nuget provider version from 2.8.5.207 to 2.8.5.208

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.8.5.207")]
-[assembly: AssemblyFileVersion("2.8.5.207")]
+[assembly: AssemblyVersion("2.8.5.208")]
+[assembly: AssemblyFileVersion("2.8.5.208")]
