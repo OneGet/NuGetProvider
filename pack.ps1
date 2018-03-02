a875fc55ec65e96ef279ae3edc03476a02304f23
Bryan Wang (PSHCT) <brywang@microsoft.com>
2018-03-02 11:05:21 -0800

    Build NuGetProvider from OneGet NuGet package and include NuGet packing script

--- pack.ps1
+++ pack.ps1
@@ -1,38 +1,146 @@
-<# Package NuGetProvider into a NuGet package #>
+<#
+Create a NuGet package from an output location
+#>
+[CmdletBinding()]
 param(
-    [Parameter(Mandatory=$true)]
+	# e.g. C:\git\nugetprovider\bin\Release
+    [Parameter(Mandatory = $true)]
     [string]
-    $Source,
+    $OutputRootDir,
 
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $false)]
+    [string]
+    $PackageId = "Microsoft.PowerShell.PackageManagement.NuGetProvider",
+
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Authors = "Microsoft",
+
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Owners = "brywang",
+
+    [Parameter(Mandatory = $false)]
+    [string[]]
+    $ReleaseNotes,
+
+    [Parameter(Mandatory = $false)]
+    [string]
+    $ReleaseNotesFile,
+
+    [Parameter(Mandatory = $false)]
     [switch]
-    $RefreshNuGetClient
+    $IncludePdbs,
+
+    [Parameter(Mandatory = $false)]
+    [switch]
+    $RefreshNuGetClient,
+
+    [Parameter(Mandatory = $false)]
+    [string]
+    $PackageOutDir,
+
+    [Parameter(Mandatory = $false)]
+    [string]
+    $PrereleaseString
 )
 
-if ((-not (Test-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath nuget.exe))) -or $RefreshNuGetClient) {
-    Write-Host "Downloading latest NuGet.exe"
-    $null = Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile (Join-Path -Path $PSScriptRoot -ChildPath nuget.exe)
+if (-not $PackageOutDir) {
+    $PackageOutDir = $PSScriptRoot
 }
 
-$nuspecContent = Get-Content (Join-Path -Path $PSScriptRoot -ChildPath "Microsoft.PackageManagement.NuGetProvider.nuspec_template") | Out-String
-$releaseNotes = Get-Content (Join-Path -Path $PSScriptRoot -ChildPath "releasenotes.md") | Out-String
-$versionFileContent = Get-Content (Join-Path -Path $PSScriptRoot -ChildPath "Properties" | Join-Path -ChildPath "AssemblyInfo.cs") | Out-String
-$versionFileContent -match "[[]assembly: AssemblyVersion[(]""(.*)""[)][]]"
-$version = $matches[1]
-Write-Verbose "Version: $version"
-$fileTemplate = "<file src=""{path}"" target=""lib\{framework}"" />"
-$files = ""
-foreach ($frameworkDir in (Get-ChildItem -Path $Source -Directory)) {
-    Write-Verbose "Framework: $(Split-Path -Path $frameworkDir -Leaf)"
-    foreach ($dll in (Get-ChildItem -Path "$($frameworkDir.FullName)\Microsoft.PackageManagement.NuGetProvider.dll")) {
-        Write-Verbose "Path: $($dll.FullName)"
-        $files += $fileTemplate.Replace("{path}", $dll.FullName).Replace("{framework}", (Split-Path -Path $frameworkDir -Leaf))
+$nugetCommand = Get-Command nuget -ErrorAction Ignore
+if ((-not $nugetCommand) -or $RefreshNuGetClient) {
+    Write-Verbose "Downloading latest NuGet.exe"
+    $null = Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile (Join-Path -Path $PSScriptRoot -ChildPath nuget.exe)
+    $nugetCommand = Get-Command nuget -ErrorAction Ignore
+}
+
+Write-Verbose "Using NuGet version: $($nugetCommand.Version)"
+$nugetPath = $nugetCommand.Source
+$tempDir = Join-Path -Path ([System.IO.Path]::GetTempPath()) -ChildPath ([System.IO.Path]::GetRandomFileName())
+$null = New-Item -Path $tempDir -ItemType Directory
+try {
+    $version = $null
+    foreach ($frameworkPathItem in (Get-ChildItem -Path $OutputRootDir -Directory)) {
+		$framework = Split-Path -Path $frameworkPathItem.FullName -Leaf
+        $libDir = Join-Path -Path $tempDir -ChildPath 'lib' | Join-Path -ChildPath $framework
+        if (-not (Test-Path -Path $libDir)) {
+            $null = New-Item -Path $libDir -ItemType Directory
+        }
+
+		$dllPath = Join-Path -Path $frameworkPathItem.FullName -ChildPath "Microsoft.PackageManagement.NuGetProvider.dll"
+		Write-Verbose "Preparing file: $($dllPath)"
+        Copy-Item -Path $dllPath -Destination $libDir
+		if (-not $version) {
+            $version = [System.Reflection.AssemblyName]::GetAssemblyName($dllPath).Version.ToString()
+		}
+		
+        if ($IncludePdbs) {
+            $dllPath = Join-Path -Path $frameworkPathItem.FullName -ChildPath "Microsoft.PackageManagement.NuGetProvider.pdb"
+			Write-Verbose "Preparing file: $($dllPath)"
+			Copy-Item -Path $dllPath -Destination $libDir
+        }
+    }
+
+    if (-not $version) {
+        throw "Couldn't discover version of package - is Microsoft.PackageManagement.dll there?"
+    }
+
+    if ($PrereleaseString) {
+        $version += "-$PrereleaseString"
+    }
+
+    $nuspecContents = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath 'NuGetProvider.nuspec') | Out-String
+    Write-Verbose "Setting Package.Id = $PackageId"
+    $nuspecContents = $nuspecContents -replace "{name}",$PackageId
+    Write-Verbose "Setting Package.Version = $version"
+    $nuspecContents = $nuspecContents -replace "{version}",$version
+    Write-Verbose "Setting Package.Authors = $Authors"
+    $nuspecContents = $nuspecContents -replace "{authors}",$Authors
+    Write-Verbose "Setting Package.Owners = $Owners"
+    $nuspecContents = $nuspecContents -replace "{owners}",$Owners
+    if ($ReleaseNotesFile) {
+        if (-not (Test-Path -Path $ReleaseNotesFile)) {
+            throw "Release notes file '$ReleaseNotesFile' does not exist"
+        }
+
+        $ReleaseNotes = Get-Content -Path $ReleaseNotesFile | Out-String
+    } else {
+        $ReleaseNotes = $ReleaseNotes | Out-String
+    }
+
+    Write-Verbose "Setting Package.ReleaseNotes = '$ReleaseNotes'"
+    $nuspecContents = $nuspecContents -replace "{releaseNotes}",$ReleaseNotes
+    $nuspecName = "temp.nuspec"
+    $nuspecContents | Out-File -FilePath (Join-Path -Path $tempDir -ChildPath $nuspecName)
+    Write-Verbose "Packing..."
+    Push-Location -Path $tempDir
+    & $nugetPath pack $nuspecName
+    Pop-Location
+    if ($LastExitCode -gt 0) {
+        throw 'NuGet.exe pack failed. See previous error messages.'
+    }
+
+    Write-Verbose "Copying package to $PackageOutDir"
+    Get-ChildItem -Path (Join-Path -Path $tempDir -ChildPath "*.nupkg") | Select-Object -First 1 | Select-Object -ExpandProperty FullName | Copy-Item -Destination $PackageOutDir
+} finally {
+    $tries = 3
+    while ((Test-Path -Path $tempDir) -and ($tries -gt 0)) {
+        try {
+            $null = Remove-Item -Path $tempDir -Recurse -Force -ErrorAction Ignore
+        } catch {
+        }
+
+        if ((Test-Path -Path $tempDir) -and ($tries -gt 0)) {
+            Start-Sleep -Milliseconds (100 * $tries)
+            $tries = $tries - 1
+        }
+    }
+
+    if (Test-Path -path $tempDir) {
+        Write-Error "Failed to remove temp directory: $tempDir"
     }
 }
 
-$nuspecContent = $nuspecContent.Replace("{version}", $version)
-$nuspecContent = $nuspecContent.Replace("{release notes}", $releaseNotes)
-$nuspecContent = $nuspecContent.Replace("{files}", $files)
-$nuspecContent | Out-File "Microsoft.PackageManagement.NuGetProvider.nuspec"
-& (Join-Path -Path $PSScriptRoot -ChildPath nuget.exe) pack "Microsoft.PackageManagement.NuGetProvider.nuspec"
-Write-Host "Done packing."
+Write-Verbose "Done"
