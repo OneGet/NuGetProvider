b4c4ff88e76c747954c0ebdc84386cc2a6cc43a0
Christoph Bergmeister <c.bergmeister@gmail.com>
2018-12-06 10:34:55 +0000

    Revert "upgrade vs-csproj to csproj2017 format and make yaml consistent"
    This reverts commit 745d41e9d5553e5ab27ae06940c0f903b8c97030.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,6 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Microsoft.PackageManagement.NuGetProvider")]
+[assembly: AssemblyDescription("NuGet Provider for OneGet")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("Microsoft.PackageManagement.NuGetProvider")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -9,3 +21,13 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9e5b0990-975f-4033-b98b-3e81dfecacf4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("3.0.0.2")]
+[assembly: AssemblyFileVersion("3.0.0.2")]
